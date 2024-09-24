7fe35cde1aa79fc21354d538396de5473072cf78
Reo <reo235711@gmail.com>
2024-09-24 11:49:49 +0900

    뼈대 완성(print로 무한루프 구조 확인)

--- character_grass.py
+++ character_grass.py
@@ -5,50 +5,16 @@ open_canvas()
 grass = load_image('grass.png')
 character = load_image('character.png')
 
-import math
+def run_Circle():
+    print('Circle')
+    pass
 
-def drawRect(x,y):
-    while(x<760):
-        clear_canvas_now()
-        grass.draw_now(400,30)
-        character.draw_now(x,y)
-        x = x + 10
-        delay(0.01)
-    while(y<550):
-        clear_canvas_now()
-        grass.draw_now(400,30)
-        character.draw_now(x,y)
-        y = y + 10
-        delay(0.01)
-    while(x>10):
-        clear_canvas_now()
-        grass.draw_now(400,30)
-        character.draw_now(x,y)
-        x = x - 10
-        delay(0.01)
-    while(y>90):
-        clear_canvas_now()
-        grass.draw_now(400,30)
-        character.draw_now(x,y)
-        y = y - 10
-        delay(0.01)
+def run_Rect():
+    print("rect")
+    pass
 
-def drawCircle(x,y,r):
-    angle = 450
-    while(angle > 90):
-        x = math.cos(angle/360*2*math.pi)*r
-        y = math.sin(angle/360*2*math.pi)*r
-        clear_canvas_now()
-        grass.draw_now(400,30)
-        character.draw_now(400 + x,300 + y)
-        angle -= 5
-        delay(0.01)
+while True:
+    run_Circle()
+    run_Rect()
 
-             
-x = 0
-y = 90
-radius = 200
-while(1):
-    drawRect(x,y);
-    drawCircle(x,y,radius);
 close_canvas()
