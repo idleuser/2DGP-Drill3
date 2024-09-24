0a9105803e1bec535390603878143e05d3d1b456
Reo <reo235711@gmail.com>
2024-09-24 12:27:21 +0900

    사각운동 기본 흐름 구조 완성

--- character_grass.py
+++ character_grass.py
@@ -11,7 +11,7 @@ def run_Circle():
     import math
 
     r,cx,cy = 300,800//2,600//2
-    for degree in range(0,360,3):
+    for degree in range(0,360,5):
         theta = math.radians(degree)
         x = r*math.cos(theta) + cx
         y = r*math.sin(theta) + cy
@@ -20,15 +20,32 @@ def run_Circle():
         boy.draw_now(x,y)
         delay(0.1)
     
+    pass
+
+def run_top():
+    print('TOP')
+    pass
+def run_right():
+    print('RIGHT')
+    pass
+def run_bottom():
+    print('BOTTOM')
+    pass
+def run_left():
+    print('LEFT')
+    pass
+def run_Rect():
+    print("Rect")
+
+    run_top()
+    run_right()
+    run_bottom()
+    run_left()
     
     pass
 
-def run_Rect():
-    print("rect")
-    pass
-
 while True:
-    run_Circle()
+    #run_Circle()
     run_Rect()
     break
 
